106b6ef42a3a172d1e92f6e42d57ab1e04b1d4e1
Punit Naik <naik.punit44@gmail.com>
2018-08-29 01:04:44 +0530

    :art: Refactored the tests

--- test/ssh_cli/login/core_test.clj
+++ test/ssh_cli/login/core_test.clj
@@ -5,14 +5,17 @@
             [ssh-cli.core :refer [exec-remote]]
             [clojure.string :refer [trim]]))
 
+(defonce ^:private user-name (System/getProperty "user.name"))
+(defonce ^:private password (trim (slurp (resource "passwd"))))
+
 (deftest passwordless-ssh-test
   (testing "NOTE: Please paste your password as text in the `resources/passwd` file and also,
                   please remove your public key from `~/.ssh/authorized_keys` if present!
             Passwordless SSH functionality test on local machine..."
     (setup-passwordless-ssh {:ip-list [{:public-ip "127.0.0.1" :private-ip "127.0.0.1"}]
-                             :user-name (System/getProperty "user.name")
-                             :password (trim (slurp (resource "passwd")))})
-    (is (= (trim (:out (exec-remote {:machine (str (System/getProperty "user.name") "@127.0.0.1")
+                             :user-name user-name
+                             :password password})
+    (is (= (trim (:out (exec-remote {:machine (str user-name "@127.0.0.1")
                                      :cmd "echo $USER"
-                                     :password (trim (slurp (resource "passwd")))})))
+                                     :password password})))
            (System/getProperty "user.name")))))
